7293ae6cc89c7af19e699a436ba776bee30e554d
Gabriele Muscogiuri <muscogiuri.gabriele@gmail.com>
2016-12-05 22:17:09 +0100

    added infoBox shortcode

--- shortcode.php
+++ shortcode.php
@@ -1,3 +1,9 @@
+<?php
+////////////////////////////
+// 1. SECTION
+// usage: [section container="" name="" calss="" id=""][/section]
+////////////////////////////
+
 function Section($options=array(), $content = null) {
 
     $defaults=shortcode_atts(array(
@@ -22,3 +28,32 @@ function Section($options=array(), $content = null) {
     return $text;
 }
 add_shortcode('section','Section');
+
+
+////////////////////////////
+// 2. INFO BOX
+// usage: [infobox container="" name="" calss="" id=""][/infobox]
+////////////////////////////
+
+function infoBox($options=array(), $content = null) {
+
+    $defaults=shortcode_atts(array(
+        'name' => '',
+        'id' => '',
+        'class' => ''
+    ), $options);
+    ob_start(); ?>
+    <div
+        <?php echo $options['id']!='' ? 'id='.$options['id'] : ''; ?>
+        <?php echo $options['class']!='' ? 'class='.$options['class'] : ''; ?>
+        <?php echo $options['name']!='' ? 'meta-id='.$options['name'] : ''; ?>
+    >
+        <?php echo $content;?>
+    </div>
+    <?php
+    $text=ob_get_contents();
+    ob_clean();
+    return $text;
+}
+add_shortcode('infobox','infoBox');
+?>
